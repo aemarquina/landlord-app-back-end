502bca83cb9af04b9988675a15204d2711e96374
aemarquina <aeamarquina@gmail.com>
2020-05-14 22:19:48 -0400

    Added Review Serializer Relationship
    - Added README

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,5 +1,4 @@
 class ReviewSerializer < ActiveModel::Serializer
-  puts 'Youre in serializer'
   attributes :id, :property, :rating, :landlord, :movein, :moveout, :description
   has_one :user
 end
